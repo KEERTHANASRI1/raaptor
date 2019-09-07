44b6f2108a496795af01ca6a27de85359be1f800
keerthanasri1 <keerthana.kamisetti@gmail.com>
2019-09-07 17:06:15 +0530

    raaptor 1st class

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="enter input";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz ="enter input";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   if (a==b)
+   {
+   }
+   else
+   {
+   }
+   cout << yes << endl;
+   return 0;
+}
